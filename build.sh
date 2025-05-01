0cc930f528a6cd204fb6fe788fceac9a63378cfa
Shovit <119912892+Virtual4087@users.noreply.github.com>
2025-05-01 17:52:09 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y chromium-browser
+playwright install chromium
+
+# Download chromedriver compatible with the installed Chromium version
+wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip -P /tmp
+unzip /tmp/chromedriver_linux64.zip -d /tmp
+mv /tmp/chromedriver /usr/local/bin/
