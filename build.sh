6dab623d3e58658557061f87502176bcab4277d9
Shovit <119912892+Virtual4087@users.noreply.github.com>
2025-05-01 20:20:26 +0545

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Download chromedriver compatible with the installed Chromium version
-wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip -P /tmp
-unzip /tmp/chromedriver_linux64.zip -d /tmp
-mv /tmp/chromedriver /usr/local/bin/
